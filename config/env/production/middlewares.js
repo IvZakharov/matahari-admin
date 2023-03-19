1d9e2c571b29a296735a177851db155c7e482551
Ivan <iv@Ivans-MacBook-Air.local>
2023-03-19 11:55:59 +0300

    some fix

--- config/env/production/middlewares.js
+++ config/env/production/middlewares.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => [
       contentSecurityPolicy: {
         directives: {
           'connect-src': ['self', 'http:', env("DO_SPACE_CDN")],
-          'img-src': ['self', 'data:', 'blob:', env("DO_SPACE_CDN"), 'https://admin.alex.villas/'],
+          'img-src': ['self', 'data:', 'blob:', env("DO_SPACE_CDN"), ''],
           'media-src': ["'self'", 'data:', 'blob:', env("DO_SPACE_CDN")]
         },
       },
